0115362f4e6b42d2244351ad662ba03b7acfe753
PARMENTIER Quentin <qparmentier@europe.corp.altran.com>
2019-12-31 13:59:06 +0100

    reworked

--- webhooks.php
+++ webhooks.php
@@ -1,105 +1,39 @@
 <?php
 
-/*$monfichier = fopen('post.txt', 'r+');
+// $monfichier = fopen('post.txt', 'r+');
 
-$post_data = file_get_contents('php://input');
+//$post_data = file_get_contents('php://input');
 
 //fputs($monfichier, json_encode($_SERVER));
-fputs($monfichier, $post_data);
-fclose($monfichier);
+// fputs($monfichier, $post_data);
+// fclose($monfichier);
+
+
+
+// If the request is a POST request then we're getting a real webhook callback.
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Default to not verified state.
+    $isVerified = FALSE;
+    // Get the signature from the headers
+    $sig = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+    // Only test if there's possibly a signature
+    if(!empty($sig)) {
+        // Read the raw POST data
+        $data = file_get_contents("php://input");
+        // Calculate a hash and prepend the algorithm to match the signature
+        $hash = 'sha1=' . hash_hmac('sha1', $data, 'PipiCaca');
+        // Compare the calculated and signature hash strings
+        $isVerified = $hash === $sig;
+    }
+    
+    $monfichier = fopen('post.txt', 'r+');
+    fputs($monfichier, $isVerified.' : '.$hash.' : '.$sig);
+    fclose($monfichier);
+
+}
 
 ?>
-
 <form method="get">
 
   <button type="submit" formaction="./efface.php"> EFFACE </button>
-</form> */
-
-<?php
-// where to log errors and successful requests
-define('LOGFILE', '/tmp/github-webhook.log');
-
-// what command to execute upon retrieval of a valid push event
-$cmd = "git clone https://seokuymi:Pcw1234.@github.com/seokuymi/webhooks.git";
-
-// the shared secret, used to sign the POST data (using HMAC with SHA1)
-$secret = 'PipiCaca';
-
-// receive POST data for signature calculation, don't change!
-$post_data = file_get_contents('php://input');
-$signature = hash_hmac('sha1', $post_data, $secret);
-
-// required data in POST body - set your targeted branch and repository here!
-$required_data = array(
-	'ref' => 'refs/heads/master',
-	'repository' => array(
-		'full_name' => 'seokuymi/webhooks',
-	),
-);
-
-// required data in headers - probably doesn't need changing
-$required_headers = array(
-	'REQUEST_METHOD' => 'POST',
-	'HTTP_X_GITHUB_EVENT' => 'push',
-	'HTTP_USER_AGENT' => 'GitHub-Hookshot/*',
-	'HTTP_X_HUB_SIGNATURE' => 'sha1=' . $signature,
-);
-
-// END OF CONFIGURATION
-
-error_reporting(0);
-
-function log_msg($msg) {
-	if(LOGFILE != '') {
-		file_put_contents(LOGFILE, $msg . "\n", FILE_APPEND);
-	}
-}
-
-function array_matches($have, $should, $name = 'array') {
-	$ret = true;
-	if(is_array($have)) {
-		foreach($should as $key => $value) {
-			if(!array_key_exists($key, $have)) {
-				log_msg("Missing: $key");
-				$ret = false;
-			}
-			else if(is_array($value) && is_array($have[$key])) {
-				$ret &= array_matches($have[$key], $value);
-			}
-			else if(is_array($value) || is_array($have[$key])) {
-				log_msg("Type mismatch: $key");
-				$ret = false;
-			}
-			else if(!fnmatch($value, $have[$key])) {
-				log_msg("Failed comparison: $key={$have[$key]} (expected $value)");
-				$ret = false;
-			}
-		}
-	}
-	else {
-		log_msg("Not an array: $name");
-		$ret = false;
-	}
-	return $ret;
-}
-
-log_msg("=== Received request from {$_SERVER['REMOTE_ADDR']} ===");
-header("Content-Type: text/plain");
-$data = json_decode($post_data, true);
-// First do all checks and then report back in order to avoid timing attacks
-$headers_ok = array_matches($_SERVER, $required_headers, '$_SERVER');
-$data_ok = array_matches($data, $required_data, '$data');
-if($headers_ok && $data_ok) {
-  $monfichier = fopen('post.txt', 'r+');
-  fputs($monfichier, "PDDDDD");
-  fclose($monfichier);
-
-	passthru($cmd);
-}
-else {
-  $monfichier = fopen('post.txt', 'r+');
-  fputs($monfichier, "AAAAAAAAA");
-  fclose($monfichier);
-	http_response_code(403);
-	die("Forbidden\n");
-}
+</form>
