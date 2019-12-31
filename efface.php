78a45c2a364763dc175cd033c3bf843db89cc280
PARMENTIER Quentin <qparmentier@europe.corp.altran.com>
2019-12-31 10:36:44 +0100

    ok

--- efface.php
+++ efface.php
@@ -1,7 +1,8 @@
 <?php
 
-unlink("post.txt");
-$monfichier = fopen('post.txt', 'r+');
+if(file_exists("post.txt")) unlink("post.txt");
+
+$monfichier = fopen('post.txt', 'w+');
 fclose($monfichier);
 
 ?>
